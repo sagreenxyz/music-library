6acc6bf08f8a3ab268a5ca214991ff77ae423d61
Scott Green <sagreenxyz@gmail.com>
2022-04-04 10:07:27 -0500

    7. rendering the data

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,14 @@
 import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
+    })
     return (
         <div>
-            <GalleryItem />
+            {display}
         </div>
     )
 }
