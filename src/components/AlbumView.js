7b2b04c8363281d504037c7046501c9941c7b327
Scott Green <sagreenxyz@gmail.com>
2022-04-04 14:23:49 -0500

    25. stub AlbumView component

---
+++ src/components/AlbumView.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+function AlbumView() {
+    const [albumData, setAlbumData] = useState([])
+
+    return (
+        <div>
+            <p>Album Data Goes Here!</p>
+        </div>
+    )
+}
+
+export default AlbumView
