ec9b5533c7da67abba86d5447c2cf9e9c5ecca8f
Scott Green <sagreenxyz@gmail.com>
2022-04-04 14:25:00 -0500

    26. adding imports to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css'
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
   let [search, setSearch] = useState('')
@@ -36,6 +39,8 @@ function App() {
       <SearchBar handleSearch={handleSearch} />
       {message}
       <Gallery data={data} />
+      <AlbumView />
+      <ArtistView />
     </div>
   );
 }
