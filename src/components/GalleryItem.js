ddc347b4263c6ec9f973bc9188b92dda435900c0
Scott Green <sagreenxyz@gmail.com>
2022-04-04 10:09:50 -0500

    8. structuring the styling for simple and detail views

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,9 +3,27 @@ import { useState } from 'react'
 function GalleryItem(props) {
     let [view, setView] = useState(false)
 
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+
     const simpleView = () => {
         return (
-            <div>
+            <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -14,7 +32,7 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
@@ -24,7 +42,7 @@ function GalleryItem(props) {
     }
 
     return (
-        <div onClick={() => setView(!view)} style={{ 'display': 'inline-block', 'border': '1px solid black' }}>
+        <div onClick={() => setView(!view)}>
             {view ? detailView() : simpleView()}
         </div>
     )
