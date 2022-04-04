6ba57e27d094347128b612af39654c5efe7661ac
Scott Green <sagreenxyz@gmail.com>
2022-04-04 14:44:35 -0500

    31. to maintain context switching from anchor tags to Link tags

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props) {
     let [view, setView] = useState(false)
@@ -35,14 +36,14 @@ function GalleryItem(props) {
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>
-                    <a href={`/artist/${props.item.artistId}`}>
+                    <Link to={`/artist/${props.item.artistId}`}>
                         {props.item.artistName}
-                    </a>
+                    </Link>
                 </h3>
                 <h3>
-                    <a href={`/album/${props.item.collectionId}`}>
+                    <Link to={`/album/${props.item.collectionId}`}>
                         {props.item.collectionName}
-                    </a>
+                    </Link>
                 </h3>
 
                 <h4>{props.item.primaryGenreName}</h4>
